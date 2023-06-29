d2fe99d777659e1ce4b2d941147b17ba9905b7cb
Rodrigo Felix <rodrigo.felix321@gmail.com>
2023-06-29 18:48:22 -0300

    show data on screen

--- src/main/java/com/example/desafio/controllers/FileController.java
+++ src/main/java/com/example/desafio/controllers/FileController.java
@@ -6,20 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
-@RequestMapping("/api")
+@Controller
 public class FileController {
 
     @Autowired
     FileService fileService;
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 
     @GetMapping("/transactions")
     public ResponseEntity<List<FileTransaction>> getAllTransactions() {
@@ -29,12 +36,15 @@ public class FileController {
     }
 
     @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes)
-            throws IOException {
-        fileService.uploadFile(file);
-
-        redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
-        return "redirect:/";
+    public String uploadFile(@RequestParam("file") MultipartFile file, Model model) {
+        try {
+            fileService.uploadFile(file);
+            List<FileTransaction> transactions = fileService.getAllTransactions();
+            model.addAttribute("transactions", transactions);
+            return "home";
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return null;
     }
 }
