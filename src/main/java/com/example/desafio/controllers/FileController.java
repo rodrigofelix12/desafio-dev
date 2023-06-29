4b0818a9df76e495a955aea9984480343b1162ba
Rodrigo Felix <rodrigo.felix321@gmail.com>
2023-06-29 12:23:11 -0300

    mapping rest controller

--- src/main/java/com/example/desafio/controllers/FileController.java
+++ src/main/java/com/example/desafio/controllers/FileController.java
@@ -1,12 +1,40 @@
 package com.example.desafio.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.desafio.model.FileTransaction;
 import com.example.desafio.services.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class FileController {
 
     @Autowired
-    private FileService fileService;
+    FileService fileService;
+
+    @GetMapping("/transactions")
+    public ResponseEntity<List<FileTransaction>> getAllTransactions() {
+        List<FileTransaction> transactions = new ArrayList<>();
+        transactions = fileService.getAllTransactions();
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes)
+            throws IOException {
+        fileService.uploadFile(file);
+
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+        return "redirect:/";
+    }
 }
