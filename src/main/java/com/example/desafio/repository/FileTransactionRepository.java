433d40605aad655630b9392399d5091d4bdb1b52
Rodrigo Felix <rodrigo.felix321@gmail.com>
2023-06-29 12:24:02 -0300

    create repository

--- src/main/java/com/example/desafio/repository/FileTransactionRepository.java
+++ src/main/java/com/example/desafio/repository/FileTransactionRepository.java
@@ -3,5 +3,5 @@ package com.example.desafio.repository;
 import com.example.desafio.model.FileTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FileRepository extends JpaRepository<FileTransaction, Long> {
+public interface FileTransactionRepository extends JpaRepository<FileTransaction, Long> {
 }
